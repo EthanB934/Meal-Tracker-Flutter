eb4978dd5064f685b4592364a2aa1a25e413313b
Ethan Brown <eb934053@gmail.com>
2026-04-27 14:34:05 -0500

    Merging branch into main

---
+++ lib/data/database_helper.dart
@@ -0,0 +1,10 @@
+class DatabaseHelper {
+  DatabaseHelper._privateConstructor();
+
+  static final DatabaseHelper _instance = DatabaseHelper._privateConstructor();
+
+  factory DatabaseHelper() {
+    return _instance;
+  }
+
+}
