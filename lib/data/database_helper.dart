4e9aa470b32ae494ab9010a2f98dd74f37556cb6
Ethan Brown <eb934053@gmail.com>
2026-04-27 15:56:17 -0500

    Created Initialize Database Method in Database Helper

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -1,3 +1,5 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 class DatabaseHelper {
   DatabaseHelper._privateConstructor();
 
@@ -7,4 +9,20 @@ class DatabaseHelper {
     return _instance;
   }
 
+  Future<Database> initDatabase () async {
+    var databasePath = await getDatabasesPath();
+    String path = join(databasePath, 'meal_tracker.db');
+
+    Database database = await openDatabase(
+       path,
+       version: 1,
+       onCreate: (Database db, int version) async {
+         await db.execute(
+           'CREATE TABLE Test (id INTEGER PRIMARY KEY, name TEXT, value INTEGER, num REAL)'
+         );
+       }
+    );
+
+    return database;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:my_flutter_application/Screens/onboarding_screen.dart';
+import 'package:my_flutter_application/data/database_helper.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  DatabaseHelper().initDatabase();
   runApp(MyApp());
 }
 
