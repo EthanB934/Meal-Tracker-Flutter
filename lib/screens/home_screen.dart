5519682d7c911d190fab5bee61b865b0fa01dd9d
Ethan Brown <eb934053@gmail.com>
2026-07-14 18:09:48 -0500

    Fixed Home Screen bug, no user parameter set

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:my_flutter_application/models/user.dart';
+import 'package:my_flutter_application/utils/greeting.dart';
 
 class HomeScreen extends HookWidget {
+  final User user;
+  const HomeScreen({
+    super.key,
+    required this.user
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text(Greeting().greet()),
       ),
       body: Center(
         child: Column(
