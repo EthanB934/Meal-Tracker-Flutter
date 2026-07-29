4d77cb833111c0836620b53250b8b91fafca3c60
Ethan Brown <eb934053@gmail.com>
2026-07-29 17:00:36 -0500

    Scaffolded Home Screen placeholder UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -51,42 +51,70 @@ class HomeScreen extends HookWidget {
       appBar: AppBar(
         title: Text(Greeting().greet()),
       ),
-      body: Card(
-        child: ListView.builder(
-          itemCount: userPreferences.length,
-          itemBuilder: (context, index) {
-            final preference = userPreferences[index];
-            final Nutrient nutrient = nutrients.firstWhere(
-                    (n) => n.id == preference.nutrientId
-            );
+        body: Column(
+          children: [
+            Text("Nutritional Summary"),
+            SizedBox(
+              height: 300,
+              child: Card(
+                child: ListView.builder(
+                  itemCount: userPreferences.length,
+                  itemBuilder: (context, index) {
+                    final preference = userPreferences[index];
+                    final Nutrient nutrient = nutrients.firstWhere(
+                            (n) => n.id == preference.nutrientId
+                    );
 
-            final double currentAmount = 9.75;
+                    final double currentAmount = 9.75;
 
-            return SizedBox(
-                height: 100,
-                width: double.infinity,
-                child: ListTile(
-                    title: Text(nutrient.name),
-                    subtitle: Text('${FloorToDecimal().floorToDecimal(((preference.goalAmount - currentAmount)).toDouble(), 2)} remaining'),
-                    trailing: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text('$currentAmount ${nutrient.unit} / ${preference.goalAmount} ${nutrient.unit}'),
-                      SizedBox(
-                        height: 10,
-                        width: 250,
-                        child: LinearProgressIndicator(
-                          value: currentAmount / preference.goalAmount,
-                        ),
-                      ),
-                    ]
-                )
+                    return SizedBox(
+                        height: 100,
+                        width: double.infinity,
+                        child: ListTile(
+                            title: Text(nutrient.name),
+                            subtitle: Text('${FloorToDecimal().floorToDecimal(((preference.goalAmount - currentAmount)).toDouble(), 2)} remaining'),
+                            trailing: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text('$currentAmount ${nutrient.unit} / ${preference.goalAmount} ${nutrient.unit}'),
+                                  SizedBox(
+                                    height: 10,
+                                    width: 250,
+                                    child: LinearProgressIndicator(
+                                      value: currentAmount / preference.goalAmount,
+                                    ),
+                                  ),
+                                ]
+                            )
+                        )
+                    );
+                  },
+                ),
+              ),
+            ),
+            Text("Today's Meals"),
+
+            SizedBox(
+              height: 300,
+              child: ListView.builder(
+                  itemCount: 3,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: Text("Neal #${index + 1}"),
+                    );
+                  }
               )
-            );
-          },
-        ),
-      )
+            ),
+
+
+            ElevatedButton(
+                onPressed: () {},
+                child: Text("Create New Meal")
+            )
+          ],
+
+        )
     );
   }
 }
