3f4f08fa20bbe4b3925ca23206268e4876db61bf
Ethan Brown <eb934053@gmail.com>
2026-07-31 09:06:18 -0500

    Created meal creation screen

--- lib/screens/meal_creation_form.dart
+++ lib/screens/meal_creation_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:my_flutter_application/models/food.dart';
 import 'package:my_flutter_application/models/user.dart';
+import 'package:my_flutter_application/services/food_service.dart';
 
 class MealCreationForm extends HookWidget {
   final String mealType;
@@ -15,7 +17,52 @@ class MealCreationForm extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final meal = mealType;
+    final foodFuture = useMemoized(() => FoodService().fetchFood());
+    final foodSnapshot = useFuture(foodFuture);
 
-    return Text("Meal: $meal");
+    if(foodSnapshot.connectionState == ConnectionState.waiting) {
+      return Scaffold(
+        body: Center(child: CircularProgressIndicator())
+      );
+    }
+
+    if(foodSnapshot.hasError) {
+      return Scaffold(
+        body: Center(child: Text("Error: ${foodSnapshot.error}"),),
+      );
+    }
+
+    final food = foodSnapshot.data ?? [];
+
+    return Scaffold(
+      appBar: AppBar(title: Text("Add Foods"),),
+      body: Column(
+        children: [
+
+          SizedBox(
+            height: (MediaQuery.heightOf(context) / 4) * 3,
+            width: MediaQuery.widthOf(context),
+            child: food.isEmpty
+            ? SizedBox(
+              height: MediaQuery.heightOf(context) / 2,
+              width: MediaQuery.widthOf(context) / 2,
+              child: Center(child: Text("No food items found"),),
+              )
+            : ListView.builder(
+              itemCount: food.length,
+              itemBuilder: (context, index) {
+                  final foodItem = food[index];
+                  return ListTile(
+                    leading: Text(foodItem.name),
+                  );
+                }
+            )
+          ),
+
+
+
+        ],
+      )
+    );
   }
 }
