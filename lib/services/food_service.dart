ade9e09e009114e92789f318f20ede9d28bbaab4
Ethan Brown <eb934053@gmail.com>
2026-07-31 08:24:29 -0500

    Created food service operations

---
+++ lib/services/food_service.dart
@@ -0,0 +1,40 @@
+import 'package:my_flutter_application/data/database_helper.dart';
+import 'package:my_flutter_application/models/food.dart';
+
+class FoodService {
+
+  Future<List<Food>> fetchFood() async {
+    List<Map<String, Object?>> results = await DatabaseHelper().fetchFood();
+    return results.map((result) => Food.fromMap(result)).toList();
+  }
+
+  Future<int> createFood(Food food) async {
+    int result = await DatabaseHelper().createFood(food);
+
+    if(result == 0) {
+      throw Exception("There was an error creating food item ${food.name}");
+    }
+
+    return result;
+  }
+
+  Future<int> updateFood(Food food) async {
+    int result = await DatabaseHelper().updateFood(food);
+
+    if(result == 0) {
+      throw Exception("There was an error updating ${food.name}");
+    }
+
+    return result;
+  }
+
+  Future<int> deleteFood(foodId) async {
+    int result = await DatabaseHelper().deleteFood(foodId);
+
+    if(result == 0) {
+      throw Exception("There was an error deleting food with id $foodId");
+    }
+
+    return result;
+  }
+}
